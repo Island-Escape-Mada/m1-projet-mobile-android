124222f177ccf6684b13b4e15c5474fc8fc78d7c
Famenontsoa <famenontsoa@outlook.com>
2023-07-31 16:38:59 +0300

    About mada activity

--- app/src/main/java/com/example/island_escape_mada/views/AboutMadaActivity.java
+++ app/src/main/java/com/example/island_escape_mada/views/AboutMadaActivity.java
@@ -1,16 +1,25 @@
 package com.example.island_escape_mada.views;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.island_escape_mada.R;
 
-public class AboutMadaActivity extends AppCompatActivity {
+public class AboutMadaActivity extends AppCompatActivity{
 
+    private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_mada);
+
+        webView = findViewById(R.id.webview_about_mada);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/about_mada.html");
     }
+
 }
