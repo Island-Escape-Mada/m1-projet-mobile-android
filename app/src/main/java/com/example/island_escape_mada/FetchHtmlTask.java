08fd9dd1b14aa6fe160dec3ce3aeee7da62d65d7
Famenontsoa <famenontsoa@outlook.com>
2023-08-01 15:21:00 +0300

    Fetching content from API

---
+++ app/src/main/java/com/example/island_escape_mada/FetchHtmlTask.java
@@ -0,0 +1,48 @@
+package com.example.island_escape_mada;
+
+import android.os.AsyncTask;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import java.io.IOException;
+
+public class FetchHtmlTask extends AsyncTask<String, Void, String> {
+
+    private FetchHtmlListener listener;
+
+    public FetchHtmlTask(FetchHtmlListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    protected String doInBackground(String... urls) {
+        if (urls.length == 0) {
+            return null;
+        }
+
+        String url = urls[0];
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            return response.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String htmlContent) {
+        if (listener != null) {
+            listener.onHtmlFetched(htmlContent);
+        }
+    }
+
+    public interface FetchHtmlListener {
+        void onHtmlFetched(String htmlContent);
+    }
+}
--- app/src/main/java/com/example/island_escape_mada/views/AboutMadaActivity.java
+++ app/src/main/java/com/example/island_escape_mada/views/AboutMadaActivity.java
@@ -1,15 +1,19 @@
 package com.example.island_escape_mada.views;
 
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.island_escape_mada.FetchHtmlTask;
 import com.example.island_escape_mada.R;
 
-public class AboutMadaActivity extends AppCompatActivity{
+public class AboutMadaActivity extends AppCompatActivity implements FetchHtmlTask.FetchHtmlListener {
 
+    private ProgressBar progressBar;
     private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +21,22 @@ public class AboutMadaActivity extends AppCompatActivity{
         setContentView(R.layout.activity_about_mada);
 
         webView = findViewById(R.id.webview_about_mada);
+        progressBar = findViewById(R.id.progressBar);
+
+        // webview
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        webView.loadUrl("file:///android_asset/about_mada.html");
+        // progress bar
+        progressBar.setVisibility(View.VISIBLE);
+        new FetchHtmlTask(this).execute(getString(R.string.api_url) + "about-mada");
     }
 
+    @Override
+    public void onHtmlFetched(String htmlContent) {
+        if (htmlContent != null){
+            webView.loadDataWithBaseURL(null, htmlContent, "text/html", "UTF-8", null);
+            // hide the progress bar
+            progressBar.setVisibility(View.GONE);
+        }
+    }
 }
