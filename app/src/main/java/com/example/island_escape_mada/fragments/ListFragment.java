f86b29945baf04305ea51b55414f3205e75858d1
Famenontsoa <famenontsoa@outlook.com>
2023-08-04 22:46:56 +0300

    Information list & detail

--- app/src/main/java/com/example/island_escape_mada/fragments/ListFragment.java
+++ app/src/main/java/com/example/island_escape_mada/fragments/ListFragment.java
@@ -1,11 +1,15 @@
 package com.example.island_escape_mada.fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.ConsoleMessage;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
 
 import androidx.fragment.app.Fragment;
@@ -37,9 +41,28 @@ public class ListFragment extends Fragment implements FetchHtmlTask.FetchHtmlLis
         webView = rootView.findViewById(R.id.webview_global_list);
         progressBar = rootView.findViewById(R.id.progressBar_global_list);
 
+        WebView.setWebContentsDebuggingEnabled(true);
+
         // webview
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        // webSettings.setAllowFileAccess(true);
+
+        // webView.setWebChromeClient(new WebChromeClient());
+
+        /**
+         * load template in the same current web view
+         */
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                // Prevent the default behavior of opening URLs externally
+                return false;
+            }
+        });
+
+
         // progress bar
         progressBar.setVisibility(View.VISIBLE);
         new FetchHtmlTask(this).execute(getString(R.string.api_url) + "list-info?info_type=" + infoType);
@@ -51,9 +74,25 @@ public class ListFragment extends Fragment implements FetchHtmlTask.FetchHtmlLis
     public void onHtmlFetched(String htmlContent) {
         if (htmlContent != null){
             String baseUrl = "file:///android_asset/";
+            System.out.println(htmlContent);
+            // webView.loadUrl("file:///android_asset/about_mada.html");
             webView.loadDataWithBaseURL(baseUrl, htmlContent, "text/html", "UTF-8", null);
+            // Inject the local JavaScript file into the WebView content
+            webView.loadUrl("javascript:var script = document.createElement('script'); " +
+                    "script.src = 'file:///android_asset/js/topimage.js'; " +
+                    "document.head.appendChild(script);");
             // hide the progress bar
             progressBar.setVisibility(View.GONE);
+        }
+    }
+
+    public boolean canWebViewGoBack() {
+        return webView != null && webView.canGoBack();
+    }
+
+    public void goBackInWebView() {
+        if (webView != null) {
+            webView.goBack();
         }
     }
 }
--- app/src/main/java/com/example/island_escape_mada/views/CoastActivity.java
+++ app/src/main/java/com/example/island_escape_mada/views/CoastActivity.java
@@ -27,4 +27,23 @@ public class CoastActivity extends AppCompatActivity {
             transaction.commit();
         }
     }
+
+    /**
+     * manage back button
+     * check if web view has history
+     * if true: get back to previous history
+     * else: perform normal operation on back button (just go back)
+     */
+    @Override
+    public void onBackPressed() {
+        // get list fragment
+        ListFragment fragment = (ListFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_container_view);
+        if (fragment != null && fragment.isVisible()) {
+            if (fragment.canWebViewGoBack()) {
+                fragment.goBackInWebView();
+            } else {
+                super.onBackPressed();
+            }
+        }
+    }
 }
