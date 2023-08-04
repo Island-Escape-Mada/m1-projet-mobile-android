8597e0f6eeb9615081c8fa7de1017dfd46225466
Nantenaina Rafidy~ <nantyrafidy@gmail.com>
2023-08-04 09:41:43 +0300

    Settings activity

---
+++ app/src/main/java/com/example/island_escape_mada/views/SettingActivity.java
@@ -0,0 +1,24 @@
+package com.example.island_escape_mada.views;
+import android.os.Bundle;
+
+import com.example.island_escape_mada.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        // Initialize UI elements and manage user preferences here
+
+        // Example: Accessing TextView in the user profile section and setting its text
+//        TextView tvUsername = findViewById(R.id.tv_username);
+//        String username = "John Doe"; // Replace this with the user's actual username
+//        tvUsername.setText(username);
+    }
+}
+
